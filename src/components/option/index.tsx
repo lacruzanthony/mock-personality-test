dfea3e9f96cd00d7f077b16ee0a1d3c3707a253a
Anthony Lacruz <anthony.lacruz@riseinteractive.com>
2022-05-31 22:00:47 -0500

    formatting, and improvements

--- src/components/option/index.tsx
+++ src/components/option/index.tsx
@@ -1,42 +1,23 @@
-import Button from "../button";
+import { IOption } from '../../interfaces';
+import Button from '../button';
 
-interface IOption {
-  cards: any;
-  cardNumber: number;
-  questionSelected: boolean;
-  setCardNumber: (cardNumber: number) => void;
-  setIsTestFinish: (setIsTestFinish: boolean) => void;
-}
-
-const Option = ({
-  cards,
-  cardNumber,
-  questionSelected,
-  setCardNumber,
-  setIsTestFinish
-}: IOption) => {
+const Option = ({ cards, cardNumber, isQuestionSelected, setCardNumber, setIsTestFinish }: IOption) => {
   return (
     <>
       <Button
         disabled={cardNumber <= 0}
-        content="Previous question"
+        content="< Previous question"
         onClick={() => (cardNumber > 0 ? setCardNumber(cardNumber - 1) : null)}
       />
       {cardNumber + 1 < cards.length && (
         <Button
-          disabled={!questionSelected}
-          onClick={() =>
-            cardNumber + 1 < cards.length ? setCardNumber(cardNumber + 1) : null
-          }
-          content="Next question"
+          disabled={!isQuestionSelected}
+          onClick={() => (cardNumber + 1 < cards.length ? setCardNumber(cardNumber + 1) : null)}
+          content="Next question >"
         />
       )}
       {cardNumber + 1 === cards.length && (
-        <Button
-          disabled={!questionSelected}
-          onClick={() => setIsTestFinish(true)}
-          content="Finish test"
-        />
+        <Button disabled={!isQuestionSelected} onClick={() => setIsTestFinish(true)} content="Finish test" />
       )}
     </>
   );
